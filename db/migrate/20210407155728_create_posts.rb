ab95299cd191e2b598069e2eda5167d7d82b5811
github username <github email address>
2021-04-07 08:57:52 -0700

    added post resource using rails scaffold generator

---
+++ db/migrate/20210407155728_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :category
+      t.string :image_link
+
+      t.timestamps
+    end
+  end
+end
