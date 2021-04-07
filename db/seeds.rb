da04f0d446bb732372adf1bd9ce0452bb3f98bdf
github username <github email address>
2021-04-07 09:10:14 -0700

    added some seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(title: "Title 1", content: "Content 1", category: "category", image_link: "")
+Post.create(title: "Title 2", content: "Content 2", category: "category", image_link: "")
+Post.create(title: "Title 3", content: "Content 3", category: "category", image_link: "")
+Post.create(title: "Title 4", content: "Content 4", category: "category", image_link: "")
